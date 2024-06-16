d1af34e310d27a941e58e01de00b9cf682e0754b
agran <agran@agran.net>
2024-06-16 10:56:05 +0300

    Add files via upload

---
+++ avtobusJZ.js
@@ -0,0 +1,298 @@
+var $svg;
+	
+$( document ).ready(function() {
+    console.log( "ready!" );
+	
+	$('#mestaSvobodnInput').val('1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19');
+	
+	var svgtxt = getFile('avtobusJZ3.svg');
+	var doc = (new DOMParser).parseFromString(svgtxt, 'image/svg+xml');
+
+	$svg = $('svg', doc);
+
+	$svg.find("#_Сидушка-"+20).attr('xlink:href', '#_СидушкаКрас');
+	$svg.find("#_Спинка-"+20).attr('xlink:href', '#_СпинкаКрас');
+	$svg.find(".Место-"+20).css('fill', '#7A7C6C');
+	$('#imgParent').prepend($svg);
+	console.log($svg.attr('id'));
+	
+	var mestaTableHtml = '';
+	
+	for (let i = 1; i <= 19; i++){
+		mestaTableHtml += "<div class=line-mesto data-mesto="+i+"><span class='mesto-n'>"+i+".</span> <span class='mesto-status mesto-status-svob'>Свободно</span> <button class=bZan>Занять</button></div>";
+	}
+	mestaTableHtml += "<div class=line-mesto data-mesto="+20+"><span class='mesto-n'>"+20+".</span> <span class='mesto-status mesto-status-zan'>Занято</span> <button class=bOsv>Освободить</button></div>";	
+	$('#mestaTable').html(mestaTableHtml);
+	
+	update();
+	
+	function update(){
+		var i = 0;
+		var mestaTxt = '';
+		$('.mesto-status').each(function(){
+			i ++;
+			if($(this).hasClass('mesto-status-svob')){
+				mestaTxt += i+',';
+				
+				$svg.find("#_Сидушка-"+i).attr('xlink:href', '#_СидушкаЗел');
+				$svg.find("#_Спинка-"+i).attr('xlink:href', '#_СпинкаЗел');
+				$svg.find(".Место-"+i).css('fill', '#f4f7e0');				
+				
+			}else{
+				$svg.find("#_Сидушка-"+i).attr('xlink:href', '#_СидушкаКрас');
+				$svg.find("#_Спинка-"+i).attr('xlink:href', '#_СпинкаКрас');
+				$svg.find(".Место-"+i).css('fill', '#7A7C6C');
+			}
+		});
+		
+		if (mestaTxt.length > 0 && mestaTxt[mestaTxt.length-1] === ","){
+			mestaTxt = mestaTxt.slice(0,-1);
+		}
+
+		$('#mestaSvobodnInput').val(mestaTxt);	
+		
+		//const XML = new XMLSerializer().serializeToString($svg[0]);
+		//const SVG64 = utf8_to_b64(XML);
+		
+		//$('img').attr('src', 'data:image/svg+xml;base64,' + SVG64);
+		
+		SVGToImage({
+			svg: $svg[0],
+			mimetype: "image/png",
+			width: 1550,
+			quality: 1
+		}).then(function(base64image){
+			$('img').attr('src', base64image);
+		}).catch(function(err){
+			console.log(err);
+		});
+		
+	}
+	
+ 	$( document ).on('click', '.bZan', function() {
+		var lineMesto = $(this).parent();
+		lineMesto.find('.mesto-status').removeClass('mesto-status-svob').addClass('mesto-status-zan').html('Занято');
+		lineMesto.find('button').removeClass('bZan').addClass('bOsv').html('Освободить');
+		
+		update();
+	});
+
+ 	$( document ).on('click', '.bOsv', function() {
+		var lineMesto = $(this).parent();
+		lineMesto.find('.mesto-status').removeClass('mesto-status-zan').addClass('mesto-status-svob').html('Свободно');
+		lineMesto.find('button').removeClass('bOsv').addClass('bZan').html('Занять');
+		
+		update();
+	});
+	
+	$( document ).on('click', 'img', function(e) {
+		//console.dir(e);
+		//$svg.trigger('click', e);
+		//console.dir($('svg')[0].contentDocument.elementFromPoint(50, 50));
+	});
+	
+	$( document ).on('dblclick', 'path', function() {
+		console.dir($(this).attr('id'));
+		var mestoN = Number($(this).attr('id').match(/\d+/)[0]);
+		console.dir(mestoN);
+		var lineMesto = $('.line-mesto[data-mesto="'+mestoN+'"]');
+		lineMesto.find('button').click();
+	});
+	
+	$( document ).on('click', '.copy', function(e) {
+
+		const canvas = document.createElement("canvas");
+		var img = $('img')[0];
+		
+		canvas.width = 1550;
+		canvas.height = 532;
+		canvas.getContext("2d").drawImage(img, 0, 0, 1550, 532);
+		canvas.toBlob((blob) => {
+		  navigator.clipboard.write([
+			  new ClipboardItem({ "image/png": blob })
+		  ]);
+		}, "image/png");
+
+
+	});
+	
+	$( document ).on('change', '#mestaSvobodnInput', function() {
+		
+		$('.line-mesto').each(function(){
+			var lineMesto = $(this);
+
+			
+			lineMesto.find('.mesto-status').removeClass('mesto-status-svob').addClass('mesto-status-zan').html('Занято');
+			lineMesto.find('button').removeClass('bZan').addClass('bOsv').html('Освободить');			
+		});
+		
+		var arrMesta = $(this).val().split(",");
+		arrMesta.forEach(function(mestoN){
+			mestoN = mestoN.trim();
+			
+			var lineMesto = $('.line-mesto[data-mesto="'+mestoN+'"]');
+
+			lineMesto.find('.mesto-status').removeClass('mesto-status-zan').addClass('mesto-status-svob').html('Свободно');
+			lineMesto.find('button').removeClass('bOsv').addClass('bZan').html('Занять');
+		
+			console.log(mestoN);
+		});
+		
+		update();
+	});
+	
+});
+
+function utf8_to_b64(str) {
+  return window.btoa(unescape(encodeURIComponent(str)));
+}
+
+function isset() {
+	// +   original by: Kevin van Zonneveld 
+	// +   improved by: FremyCompany
+	// +   improved by: Onno Marsman
+	// *     example 1: isset( undefined, true);
+	// *     returns 1: false
+	// *     example 2: isset( 'Kevin van Zonneveld' );
+	// *     returns 2: true
+
+	var a = arguments,
+		l = a.length,
+		i = 0;
+
+	if (l === 0) {
+		throw new Error('Empty isset');
+	}
+
+	while (i !== l) {
+		if (typeof(a[i]) == 'undefined' || a[i] === null) {
+			return false;
+		} else {
+			i++;
+		}
+	}
+	return true;
+}
+
+function getFile(strUrl, type) {
+	
+	if(!isset(type)){
+		type = 'text';
+	}
+	
+	var fileReturn;
+	
+	jQuery.ajax({
+		url: strUrl,
+		dataType: type,
+		success: function(data) {
+			fileReturn = data;
+		},
+		async: false
+	});
+	
+	return fileReturn;
+}
+
+function addZero(n) {
+	return (parseInt(n) < 10 ? "0" : "") + parseInt(n);
+}
+function pad(n) { return ("00000000" + n).substr(-8); }
+function natural_expand(a) { return a.replace(/\d+/g, pad) };
+function natural_compare(a, b) {
+    return natural_expand(a).localeCompare(natural_expand(b));
+}
+
+function isInt(value) {
+  return !isNaN(value) &&
+         parseInt(Number(value)) == value &&
+         !isNaN(parseInt(value, 10));
+}
+
+function hasNumber(myString) {
+  return /\d/.test(myString);
+}
+
+function SVGToImage(settings){
+    let _settings = {
+        svg: null,
+        // Usually all SVG have transparency, so PNG is the way to go by default
+        mimetype: "image/png",
+        quality: 0.92,
+        width: "auto",
+        height: "auto",
+        outputFormat: "base64"
+    };
+
+    // Override default settings
+    for (let key in settings) { _settings[key] = settings[key]; }
+
+    return new Promise(function(resolve, reject){
+        let svgNode;
+
+        // Create SVG Node if a plain string has been provided
+        if(typeof(_settings.svg) == "string"){
+            // Create a non-visible node to render the SVG string
+            let SVGContainer = document.createElement("div");
+            SVGContainer.style.display = "none";
+            SVGContainer.innerHTML = _settings.svg;
+            svgNode = SVGContainer.firstElementChild;
+        }else{
+            svgNode = _settings.svg;
+        }
+
+        let canvas = document.createElement('canvas');
+        let context = canvas.getContext('2d'); 
+
+        let svgXml = new XMLSerializer().serializeToString(svgNode);
+        let svgBase64 = "data:image/svg+xml;base64," + utf8_to_b64(svgXml);
+
+        const image = new Image();
+
+        image.onload = function(){
+            let finalWidth, finalHeight;
+
+            // Calculate width if set to auto and the height is specified (to preserve aspect ratio)
+            if(_settings.width === "auto" && _settings.height !== "auto"){
+                finalWidth = (this.width / this.height) * _settings.height;
+            // Use image original width
+            }else if(_settings.width === "auto"){
+                finalWidth = this.naturalWidth;
+            // Use custom width
+            }else{
+                finalWidth = _settings.width;
+            }
+
+            // Calculate height if set to auto and the width is specified (to preserve aspect ratio)
+            if(_settings.height === "auto" && _settings.width !== "auto"){
+                finalHeight = (this.height / this.width) * _settings.width;
+            // Use image original height
+            }else if(_settings.height === "auto"){
+                finalHeight = this.naturalHeight;
+            // Use custom height
+            }else{
+                finalHeight = _settings.height;
+            }
+
+            // Define the canvas intrinsic size
+            canvas.width = finalWidth;
+            canvas.height = finalHeight;
+
+            // Render image in the canvas
+            context.drawImage(this, 0, 0, finalWidth, finalHeight);
+
+            if(_settings.outputFormat == "blob"){
+                // Fullfil and Return the Blob image
+                canvas.toBlob(function(blob){
+                    resolve(blob);
+                }, _settings.mimetype, _settings.quality);
+            }else{
+                // Fullfil and Return the Base64 image
+                resolve(canvas.toDataURL(_settings.mimetype, _settings.quality));
+            }
+        };
+
+        // Load the SVG in Base64 to the image
+        image.src = svgBase64;
+    });
+}
